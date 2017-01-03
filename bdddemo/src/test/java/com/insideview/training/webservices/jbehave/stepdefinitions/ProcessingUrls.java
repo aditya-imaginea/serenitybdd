06101190b6a131c3f2a5c50bc110c5d49d18dde7
Aditya_Palagummi <aditya2510@gmail.com>
2017-01-03 11:16:46 +0530

    intial cut

--- bdddemo/src/test/java/com/insideview/training/webservices/jbehave/stepdefinitions/ProcessingUrls.java
+++ bdddemo/src/test/java/com/insideview/training/webservices/jbehave/stepdefinitions/ProcessingUrls.java
@@ -16,23 +16,24 @@ public class ProcessingUrls {
 	@Steps
 	UrlShortenerSteps urlShortener;
 
-	@Given("a url <providedUrl>")
+	@Given("a url <providedURL>")
 	public void givenAUrl(String providedUrl) {
 		this.providedUrl = providedUrl;
 	}
 
-	@When("I request the shortened form of this url")
+	/*@When("I request the shortened form of this url")
 	public void shortenUrl() {
 		returnedMessage = urlShortener.shorten(providedUrl);
-	}
+	}*/
 
 	@When("I request the expanded form of this url")
 	public void expandUrl() {
+		System.out.println(providedUrl);
 		returnedMessage = urlShortener.expand(providedUrl);
 	}
 
-	@Then("the shortened form should be <expectedUrl>")
-	public void shortenedFormShouldBe(String expectedUrl) throws JSONException {
-		urlShortener.response_should_contain_shortened_url(returnedMessage, expectedUrl);
+	@Then("the long form should be <expectedURL>")
+	public void expandedFormShouldBe(String expectedUrl) throws JSONException {
+		urlShortener.response_should_contain_long_url(returnedMessage, expectedUrl);
 	}
 }
--- bdddemo/src/test/java/com/insideview/training/webservices/jbehave/steps/UrlShortenerSteps.java
+++ bdddemo/src/test/java/com/insideview/training/webservices/jbehave/steps/UrlShortenerSteps.java
@@ -18,31 +18,33 @@ public class UrlShortenerSteps extends ScenarioSteps {
 	private static final long serialVersionUID = 5456019235449118705L;
 	RestTemplate restTemplate;
 
-    public UrlShortenerSteps() {
-        restTemplate = new RestTemplate();
-    }
+	public UrlShortenerSteps() {
+		restTemplate = new RestTemplate();
+	}
 
-    @Step("longUrl={0}")
-    public String shorten(String providedUrl) {
-        Map<String, String> urlForm = new HashMap<String, String>();
-        urlForm.put("longUrl", providedUrl);
-        return restTemplate.postForObject("https://www.googleapis.com/urlshortener/v1/url", urlForm, String.class);
-    }
+	@Step("longUrl={0}")
+	public String shorten(String providedUrl) {
+		Map<String, String> urlForm = new HashMap<String, String>();
+		urlForm.put("longUrl", providedUrl);
+		return restTemplate.postForObject("https://www.googleapis.com/urlshortener/v1/url", urlForm, String.class);
+	}
 
-    @Step("shortUrl={0}")
-    public String expand(String providedUrl) {
-        return restTemplate.getForObject("https://www.googleapis.com/urlshortener/v1/url?shortUrl={shortUrl}", String.class, providedUrl);
-    }
+	@Step("shortUrl={0}")
+	public String expand(String providedUrl) {
 
-    @Step
-    public void response_should_contain_shortened_url(String returnedMessage, String expectedUrl) throws JSONException {
-        String expectedJSONMessage = "{'id':'" + expectedUrl + "'}";
-        JSONAssert.assertEquals(expectedJSONMessage, returnedMessage, JSONCompareMode.LENIENT);
-    }
+		return restTemplate.getForObject("https://www.googleapis.com/urlshortener/v1/url?shortUrl={shortUrl}",
+				String.class, providedUrl);
+	}
 
-    @Step
-    public void response_should_contain_long_url(String returnedMessage, String expectedUrl) throws JSONException {
-        String expectedJSONMessage = "{'longUrl':'" + expectedUrl + "'}";
-        JSONAssert.assertEquals(expectedJSONMessage, returnedMessage, JSONCompareMode.LENIENT);
-    }
+	@Step
+	public void response_should_contain_shortened_url(String returnedMessage, String expectedUrl) throws JSONException {
+		String expectedJSONMessage = "{'id':'" + expectedUrl + "'}";
+		JSONAssert.assertEquals(expectedJSONMessage, returnedMessage, JSONCompareMode.LENIENT);
+	}
+
+	@Step
+	public void response_should_contain_long_url(String returnedMessage, String expectedUrl) throws JSONException {
+		String expectedJSONMessage = "{'longUrl':'" + expectedUrl + "'}";
+		JSONAssert.assertEquals(expectedJSONMessage, returnedMessage, JSONCompareMode.LENIENT);
+	}
 }
